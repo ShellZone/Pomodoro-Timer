0cda5c9761c7bce7aa41eb6255f2190cf75fe8a4
Your Name <shellzzonez@gmail.com>
2020-08-11 15:35:23 +0100

    finished barebones started script

---
+++ main.js
@@ -0,0 +1,10 @@
+// Sets min and sec to var
+var minutes = 25;
+var seconds = "00";
+
+// function to call onload sets innerhtml to the variables
+function template(){
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+}
+
