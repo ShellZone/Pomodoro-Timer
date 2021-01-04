9158c9c9a5773c4fb06e139166b67205d0d2c1ac
Roshel Samarasinghe <samarasinghe.rt@googlemail.com>
2021-01-04 16:21:57 +0000

    added reset option and sounds

--- main.js
+++ main.js
@@ -18,9 +18,36 @@ function start(){
 
     minutes = 24;
     seconds = 59;
+
     document.getElementById("minutes").innerHTML = minutes;
     document.getElementById("seconds").innerHTML = seconds;
 
     var minutes_interval = setInterval(minutesTimer, 60000);
-    var seconds_interval = setInterval(minutesTimer, 1000);
+    var seconds_interval = setInterval(secondsTimer, 1000);
+
+    function minutesTimer(){
+        minutes = minutes - 1;
+        document.getElementById("minutes").innerHTML = minutes;
+    }
+    function secondsTimer(){
+        seconds = seconds - 1;
+        document.getElementById("seconds").innerHTML = seconds;
+
+        // When it hits 0 reset then show the message 
+
+        if(seconds <= 0{
+            if(minutes <= 0){
+                // Used Clear interval To stop Time
+                clearInterval(minutes_interval);
+                clearInterval(seconds_interval);
+
+                document.getElementById("done").innerHTML = "Session Completed Take a break!!";
+
+                document.getElementById("done").classList.add("show_message");
+
+                pen.play(); 
+            }
+            seconds = 60
+        }
+    }
 }
