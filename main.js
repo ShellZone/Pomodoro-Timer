09e2acfbd90f2534d88b2758dc2d3ba590252f8c
Roshel Samarasinghe <samarasinghe.rt@googlemail.com>
2021-01-05 20:44:17 +0000

    finished added a few quotes

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ var seconds = "00";
 
 let output = document.getElementById('output');
 let quote = 
-    [ "hey","yo"
+    [ "“The secret of getting ahead is getting started.” – Mark Twain", "“The best time to plant a tree was 20 years ago. The second best time is now.” – Chinese Proverb","“It’s hard to beat a person who never gives up.” – Babe Ruth",
+    "“I wake up every morning and think to myself, ‘how far can I push this company in the next 24 hours.’” – Leah Busque","“If people are doubting how far you can go, go so far that you can’t hear them anymore.” – Michele Ruiz",
+    "“Write it. Shoot it. Publish it. Crochet it, sauté it, whatever. MAKE.” – Joss Whedon","“Everything you can imagine is real.”― Pablo Picasso","“Do one thing every day that scares you.”― Eleanor Roosevelt",
+    "“It’s no use going back to yesterday, because I was a different person then.”― Lewis Carroll"
 ];
 
 var click = new Audio("./sounds/pen.mp3");
